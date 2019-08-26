845911f84bb003d232548308a982bbea9b12b5d9
Michael Chen <cwchen123@gmail.com>
2019-08-26 21:30:25 +0800

    Remove unneeded code

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 
 	"github.com/julienschmidt/httprouter"
-	"github.com/rs/cors"
 	log "github.com/sirupsen/logrus"
 	"github.com/urfave/negroni"
 )
@@ -91,18 +90,10 @@ func main() {
 	// Handle HTTP 404
 	mux.NotFound = http.HandlerFunc(notFoundHandler)
 
-	// Handle CORS policy
-	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000"},
-		AllowCredentials: true,
-		Debug:            true,
-	})
-
 	/* Create the logger for the web application. */
 	l := log.New()
 
 	n := negroni.New()
-	n.Use(c)
 	n.Use(negronilogrus.NewMiddlewareFromLogger(l, "web"))
 	n.UseHandler(mux)
 
